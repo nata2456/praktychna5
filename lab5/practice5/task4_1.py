1a99e09d697ced867a23dc47f0bf2cf401d2e000
nata2456 <you@example.com>
2019-11-18 20:50:02 +0200

    changed

--- lab5/practice5/task4_1.py
+++ lab5/practice5/task4_1.py
@@ -1,15 +1,15 @@
-# variant1
-# xi = x(i-1) + 2*i
-i = int(input("n = "))
-
+" variant1 "
+" xi = x(i-1) + 2*i"
+" n = int(input(""))"
+n = int(input("n = "))
+i = 1
 x0 = 1
+while i <= n:
+    xi = x0 + i * 2
+    x0 = xi
+    i += 1
 
-if i == 0:
-    print(1)
-else:
-    for j in range(i-(i-1)):
-           xi = x0 + 2*i
-           x0 = xi
-           print("x(i) = {0}".format(xi))
+
+print("x(i) = {0}".format(xi))
 
 
