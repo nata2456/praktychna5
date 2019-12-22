962c1896cae6f59c966faef67dbf6ca868670eb3
nata2456 <you@example.com>
2019-12-22 20:57:59 +0200

    lab11

---
+++ lab11/lab11task1.py
@@ -0,0 +1,46 @@
+import math
+class TVector2D:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def set_x(self, arg):
+        self.x = arg
+
+    def x(self):
+         return self.x
+
+    def set_y(self, arg):
+        self.y = arg
+
+    def y(self):
+        return self.x
+
+    def square_length(self):
+        return (self.x * self.x) + (self.y * self.y)
+
+    def length(self):
+        return math.sqrt(self.square_length())
+
+    def normalize(self):
+        length = self.length()
+        if length == 0.0:
+            return TVector2D(0, 0)
+        return TVector2D(self.x / length, self.y / length)
+
+    def __add__(self, vec):
+        return TVector2D(self + vec.x, self + vec.y)
+
+    def __sub__(self, vec):
+        return TVector2D(self - vec.x, self - vec.y)
+
+    def __and__(self, vec):
+        return self.x * vec.x + self.y * vec.y
+    def __str__(self):
+        return "TVector2D: {0}, {1}".format(self.x, self.y)
+
+v = TVector2D(3.8,4.9)
+print(v)
+print('Length = ', v.length())
+print('Normalize = ', v.normalize())
+
